75220f83b141f83e8e31c2019792ddc493ba7801
kenneth <kennethchau@cpp.edu>
2025-09-17 23:29:46 -0700

    model exports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,4 +20,10 @@ const Home: NextPage = () => {
     );
 };
 
+export { default as User } from './User';
+export { default as Hangout } from './Hangout';  
+export { default as RSVP } from './RSVP';
+export type { IUser } from './User';
+export type { IHangout } from './Hangout';
+export type { IRSVP } from './RSVP';
 export default Home;
