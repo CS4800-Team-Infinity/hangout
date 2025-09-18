1f41d809ec990410cccf4871818b3c5fe28d856f
kenneth <kennethchau@cpp.edu>
2025-09-17 22:38:53 -0700

    mongodb connection cache util

---
+++ src/lib/connect.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "please define the MONGODB_URI env variable inside .env.local"
+  );
+}
+
+// cache connection to prevent reconnections in serverless envs
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+async function connect() {
+    if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
+export default connect;
