e58f859f12c621b6072006db063e27be6c0c0487
CranJam <kiranoel08@gmail.com>
2025-10-06 23:21:00 -0700

    Removed unnecessary directory I created

--- src/pages/api/notifications/notif.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-// Just a place holder this will be expanded on for notifications
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'Placeholder for notifications and me showing I can do task 3 in 3A' });
-}
