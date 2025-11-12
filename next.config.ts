7992e6f44dc59699e4c3479d46b99f5413493cf5
datorres335 <datorres335@gmail.com>
2025-11-11 22:49:42 -0800

    fixing default picture error shown at events/create page when deployed

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,20 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
+    // Increase deviceSizes and imageSizes for better optimization
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    // Set formats
+    formats: ['image/webp'],
+    // Disable minimumCacheTTL for development
+    minimumCacheTTL: 60,
   },
 };
 
