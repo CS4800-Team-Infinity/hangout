b2ab83b2163c7a44dca2dc7577d0607d90ea0866
Tam Tran <itistamtran@gmail.com>
2025-11-12 12:35:50 -0800

    update next.config to load external images correctly

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     // Temporary setup for development:
     // Allow loading images directly from any HTTPS URL (e.g., user-pasted links).
